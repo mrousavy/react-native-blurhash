196893bb37e69a9633f17f99d16af1f8e3c7b39f
Marc Rousavy <marcrousavy@hotmail.com>
2020-06-15 11:51:09 +0200

    Add demo

--- example/App.js
+++ example/App.js
@@ -9,16 +9,15 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 import Blurhash from 'react-native-blurhash';
 
 const BLURHASH_ONE = 'LEHV6nWB2yk8pyo0adR*.7kCMdnj';
 const BLURHASH_TWO = 'LGFFaXYk^6#M@-5c,1J5@[or[Q6.';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   state = {
-    status: 'starting',
-    message: '--',
+    blurhash: 'LGFFaXYk^6#M@-5c,1J5@[or[Q6.',
   };
   componentDidMount() {}
   render() {
@@ -27,9 +26,19 @@ export default class App extends Component<{}> {
         <Blurhash
           width={300}
           height={200}
-          blurhash={BLURHASH_ONE}
+          blurhash={this.state.blurhash}
           punch={1}
-          style={styles.blurhash}
+          style={styles.blurhashImage}
+        />
+        <TextInput
+          value={this.state.blurhash}
+          placeholder="Blurhash"
+          onChangeText={(text) => {
+            this.setState({
+              blurhash: text,
+            });
+          }}
+          style={styles.blurhashTextInput}
         />
       </View>
     );
@@ -43,8 +52,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  blurhash: {
+  blurhashImage: {
     // Custom styling for width height etc here
+  },
+  blurhashTextInput: {
+    marginTop: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'purple',
+    width: '70%',
+    height: 35,
+    paddingHorizontal: 20,
+    textAlign: 'center',
   },
   welcome: {
     fontSize: 20,
