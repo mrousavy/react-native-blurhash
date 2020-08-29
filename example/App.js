1b2a5753ac4706d366796222fe3a8b5e60706cf1
Marc Rousavy <marcrousavy@hotmail.com>
2020-08-29 17:58:51 +0200

    Rewrite App to functional component

--- example/App.js
+++ example/App.js
@@ -8,7 +8,7 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {Component} from 'react';
+import React, {useCallback, useState, useMemo} from 'react';
 import {
   StyleSheet,
   View,
@@ -23,91 +23,80 @@ import {
 } from 'react-native';
 import {Blurhash} from 'react-native-blurhash';
 
-export default class App extends Component {
-  state = {
-    blurhash: 'LGFFaXYk^6#M@-5c,1J5@[or[Q6.',
-    decodeAsync: true,
-    encodingImageUri: 'https://blurha.sh/assets/images/img4.jpg',
-    isEncoding: false,
-  };
-  componentDidMount() {}
+export default function App() {
+  //#region State & Props
+  const [blurhash, setBlurhash] = useState('LGFFaXYk^6#M@-5c,1J5@[or[Q6.');
+  const [decodeAsync, setDecodeAsync] = useState(true);
+  const [encodingImageUri, setEncodingImageUri] = useState(
+    'https://blurha.sh/assets/images/img4.jpg',
+  );
+  const [isEncoding, setIsEncoding] = useState(false);
+  //#endregion
 
-  async startEncoding() {
+  //#region Memos
+  const buttonOpacity = useMemo(
+    () => (encodingImageUri.length < 5 || isEncoding ? 0.5 : 1),
+    [encodingImageUri.length, isEncoding],
+  );
+  //#endregion
+
+  //#region Callbacks
+  const startEncoding = useCallback(async () => {
     try {
-      if (this.state.encodingImageUri.length < 5) {
+      if (encodingImageUri.length < 5) {
         return;
       }
-      this.setState({
-        isEncoding: true,
-      });
-      const blurhash = await Blurhash.encode(this.state.encodingImageUri, 4, 3);
-      this.setState({
-        blurhash: blurhash,
-        isEncoding: false,
-      });
+      setIsEncoding(true);
+      const _blurhash = await Blurhash.encode(encodingImageUri, 4, 3);
+      setBlurhash(_blurhash);
+      setIsEncoding(false);
     } catch (e) {
-      this.setState({
-        isEncoding: false,
-      });
+      setIsEncoding(false);
       Alert.alert('Encoding error', e);
     }
-  }
+  }, [encodingImageUri]);
+  //#endregion
 
-  render() {
-    const buttonOpacity =
-      this.state.encodingImageUri.length < 5 || this.state.isEncoding ? 0.5 : 1;
-    return (
-      <>
-        <StatusBar backgroundColor="rgba(100, 0, 100, 0.6)" />
-        <SafeAreaView style={styles.container}>
-          <Blurhash
-            blurhash={this.state.blurhash}
-            decodeWidth={32}
-            decodeHeight={32}
-            decodePunch={1}
-            decodeAsync={this.state.decodeAsync}
-            style={styles.blurhashImage}
-            resizeMode="cover"
-          />
-          <TextInput
-            value={this.state.blurhash}
-            placeholder="Blurhash"
-            onChangeText={(text) => {
-              this.setState({
-                blurhash: text,
-              });
-            }}
-            style={styles.textInput}
-          />
-          {/* To test if `decodeAsync` really doesn't block the UI thread, you can press this Touchable and see it reacting. */}
-          <View style={styles.row}>
-            <Text style={styles.text}>Decode Async:</Text>
-            <Switch
-              value={this.state.decodeAsync}
-              onValueChange={(v) => this.setState({decodeAsync: v})}
-            />
-          </View>
-          <TextInput
-            value={this.state.encodingImageUri}
-            placeholder="Image URL to encode"
-            onChangeText={(text) => {
-              this.setState({
-                encodingImageUri: text,
-              });
-            }}
-            style={styles.textInput}
-          />
-          <TouchableOpacity
-            style={[styles.encodeButton, {opacity: buttonOpacity}]}
-            disabled={this.state.encodingImageUri.length < 5}
-            onPress={this.startEncoding.bind(this)}>
-            {!this.state.isEncoding && <Text>Encode</Text>}
-            {this.state.isEncoding && <ActivityIndicator color="black" />}
-          </TouchableOpacity>
-        </SafeAreaView>
-      </>
-    );
-  }
+  return (
+    <>
+      <StatusBar backgroundColor="rgba(100, 0, 100, 0.6)" />
+      <SafeAreaView style={styles.container}>
+        <Blurhash
+          blurhash={blurhash}
+          decodeWidth={32}
+          decodeHeight={32}
+          decodePunch={1}
+          decodeAsync={decodeAsync}
+          style={styles.blurhashImage}
+          resizeMode="cover"
+        />
+        <TextInput
+          value={blurhash}
+          placeholder="Blurhash"
+          onChangeText={setBlurhash}
+          style={styles.textInput}
+        />
+        {/* To test if `decodeAsync` really doesn't block the UI thread, you can press this Touchable and see it reacting. */}
+        <View style={styles.row}>
+          <Text style={styles.text}>Decode Async:</Text>
+          <Switch value={decodeAsync} onValueChange={setDecodeAsync} />
+        </View>
+        <TextInput
+          value={encodingImageUri}
+          placeholder="Image URL to encode"
+          onChangeText={setEncodingImageUri}
+          style={styles.textInput}
+        />
+        <TouchableOpacity
+          style={[styles.encodeButton, {opacity: buttonOpacity}]}
+          disabled={encodingImageUri.length < 5}
+          onPress={startEncoding}>
+          {!isEncoding && <Text>Encode</Text>}
+          {isEncoding && <ActivityIndicator color="black" />}
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
