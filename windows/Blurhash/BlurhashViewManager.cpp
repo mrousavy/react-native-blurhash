d3d4a639f22ec8635f86aa00c5216da188dd603a
mrousavy <mrousavy@donotreply.github.com>
2020-08-30 14:02:47 +0200

    Update BlurhashViewManager.cpp

--- windows/Blurhash/BlurhashViewManager.cpp
+++ windows/Blurhash/BlurhashViewManager.cpp
@@ -7,92 +7,76 @@
 
 namespace winrt
 {
-    using namespace Microsoft::ReactNative;
-    using namespace Windows::Foundation;
-    using namespace Windows::Foundation::Collections;
-    using namespace Windows::UI;
-    using namespace Windows::UI::Xaml;
-    using namespace Windows::UI::Xaml::Controls;
+	using namespace Microsoft::ReactNative;
+	using namespace Windows::Foundation;
+	using namespace Windows::Foundation::Collections;
+	using namespace Windows::UI;
+	using namespace Windows::UI::Xaml;
+	using namespace Windows::UI::Xaml::Controls;
 }
 
 namespace winrt::Blurhash::implementation
 {
-    // IViewManager
-    hstring BlurhashViewManager::Name() noexcept
-    {
-        return L"BlurhashView";
-    }
+	// IViewManager
+	hstring BlurhashViewManager::Name() noexcept
+	{
+		return L"BlurhashView";
+	}
 
-    FrameworkElement BlurhashViewManager::CreateView() noexcept
-    {
-        _blurhashView = *winrt::make_self<BlurhashView>(_reactContext);
-        return _blurhashView;
-    }
+	FrameworkElement BlurhashViewManager::CreateView() noexcept
+	{
+		_blurhashView = *winrt::make_self<BlurhashView>(_reactContext);
+		return _blurhashView.GetView();
+	}
 
-    // IViewManagerWithNativeProperties
-    IMapView<hstring, ViewManagerPropertyType> BlurhashViewManager::NativeProps() noexcept
-    {
-        auto nativeProps = winrt::single_threaded_map<hstring, ViewManagerPropertyType>();
+	// IViewManagerWithNativeProperties
+	IMapView<hstring, ViewManagerPropertyType> BlurhashViewManager::NativeProps() noexcept
+	{
+		auto nativeProps = winrt::single_threaded_map<hstring, ViewManagerPropertyType>();
 
-        nativeProps.Insert(L"blurhash", ViewManagerPropertyType::String);
-        nativeProps.Insert(L"decodeWidth", ViewManagerPropertyType::Number);
-        nativeProps.Insert(L"decodeHeight", ViewManagerPropertyType::Number);
-        nativeProps.Insert(L"decodePunch", ViewManagerPropertyType::Number);
-        nativeProps.Insert(L"decodeAsync", ViewManagerPropertyType::Boolean);
-        nativeProps.Insert(L"resizeMode", ViewManagerPropertyType::String);
+		nativeProps.Insert(L"blurhash", ViewManagerPropertyType::String);
+		nativeProps.Insert(L"decodeWidth", ViewManagerPropertyType::Number);
+		nativeProps.Insert(L"decodeHeight", ViewManagerPropertyType::Number);
+		nativeProps.Insert(L"decodePunch", ViewManagerPropertyType::Number);
+		nativeProps.Insert(L"decodeAsync", ViewManagerPropertyType::Boolean);
+		nativeProps.Insert(L"resizeMode", ViewManagerPropertyType::String);
 
-        return nativeProps.GetView();
-    }
+		return nativeProps.GetView();
+	}
 
-    void BlurhashViewManager::UpdateProperties(
-        FrameworkElement const& view,
-        IJSValueReader const& propertyMapReader) noexcept
-    {
-        if (auto control = view.try_as<winrt::Blurhash::implementation::BlurhashView>())
-        {
-            // TODO: Properties
-            const JSValueObject& propertyMap = JSValue::ReadObjectFrom(propertyMapReader);
+	void BlurhashViewManager::UpdateProperties(
+		FrameworkElement const& view,
+		IJSValueReader const& propertyMapReader) noexcept
+	{
+		if (auto blurhashView = view.try_as<winrt::Blurhash::implementation::BlurhashView>())
+		{
+			// TODO: Properties
+			const JSValueObject& propertyMap = JSValue::ReadObjectFrom(propertyMapReader);
 
-            for (auto const& pair : propertyMap)
-            {
-                auto const& propertyName = pair.first;
-                auto const& propertyValue = pair.second;
+			for (auto const& pair : propertyMap)
+			{
+				auto const& propertyName = pair.first;
+				auto const& propertyValue = pair.second;
 
-                if (propertyName == "label")
-                {
-                    if (propertyValue != nullptr)
-                    {
-                        auto const& value = winrt::box_value(winrt::to_hstring(propertyValue.String()));
-                        control.SetValue(winrt::Blurhash::BlurhashView::LabelProperty(), propertyValue);
-                    }
-                    else
-                    {
-                        control.ClearValue(winrt::Blurhash::BlurhashView::LabelProperty());
-                    }
-                }
-                else if (propertyName == "color")
-                {
-                    if (auto value = propertyValue.To<Brush>())
-                    {
-                        control.SetValue(Control::ForegroundProperty(), value);
-                    }
-                    else
-                    {
-                        control.ClearValue(Control::ForegroundProperty());
-                    }
-                }
-                else if (propertyName == "backgroundColor")
-                {
-                    if (auto value = propertyValue.To<Brush>())
-                    {
-                        control.SetValue(Control::BackgroundProperty(), value);
-                    }
-                    else
-                    {
-                        control.ClearValue(Control::BackgroundProperty());
-                    }
-                }
-            }
-        }
-    }
+				switch (propertyName)
+				{
+					case "blurhash":
+						if (propertyValue != nullptr)
+						{
+							auto color = propertyValue.To<winrt::Color>();
+							blurhashView.DefaultBackgroundColor(color);
+							/*
+							auto const& value = winrt::box_value(winrt::to_hstring(propertyValue.String()));
+							blurhashView.SetValue(winrt::Blurhash::BlurhashView::LabelProperty(), propertyValue);
+							*/
+						}
+						else
+						{
+							//blurhashView.ClearValue(winrt::Blurhash::BlurhashView::LabelProperty());
+						}
+						break;
+				}
+			}
+		}
+	}
 }
