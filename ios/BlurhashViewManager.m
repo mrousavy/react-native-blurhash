6b44e039fd08f963ca29743f1a6dc32b96577c47
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-04 11:31:40 +0200

    [iOS] Export View Properties (Events)

--- ios/BlurhashViewManager.m
+++ ios/BlurhashViewManager.m
@@ -1,5 +1,5 @@
 #import "BlurhashBridge.h"
-#import "React/RCTViewManager.h"
+#import <React/RCTViewManager.h>
 
 @interface RCT_EXTERN_REMAP_MODULE(BlurhashView, BlurhashViewManager, RCTViewManager)
 
@@ -8,8 +8,13 @@ RCT_EXPORT_VIEW_PROPERTY(decodeWidth, NSNumber);
 RCT_EXPORT_VIEW_PROPERTY(decodeHeight, NSNumber);
 RCT_EXPORT_VIEW_PROPERTY(decodePunch, NSNumber);
 RCT_EXPORT_VIEW_PROPERTY(decodeAsync, BOOL);
+
 RCT_EXPORT_VIEW_PROPERTY(resizeMode, NSString);
 RCT_EXPORT_VIEW_PROPERTY(borderRadius, NSNumber);
+
+RCT_EXPORT_VIEW_PROPERTY(onLoadStart, RCTDirectEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onLoadEnd, RCTDirectEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onLoadError, RCTDirectEventBlock);
 
 RCT_EXTERN_METHOD(createBlurhashFromImage:(nonnull NSString *)imageUri componentsX:(nonnull NSNumber *)componentsX componentsY:(nonnull NSNumber *)componentsY resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject);
 
