420252ca4c21880e291e6274bf8bc9e6b3267dba
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-04 14:00:58 +0200

    Fix Blurhash-macos target project

---
+++ macos/Blurhash-macos/Blurhash_macos.h
@@ -0,0 +1,13 @@
+//
+//  Blurhash_macos.h
+//  Blurhash-macos
+//
+//  Created by Marc Rousavy on 04.09.20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Blurhash_macos : NSObject
+
+@end
