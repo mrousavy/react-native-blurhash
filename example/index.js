71400106d8cb23913292a1e0fd4e94a5a48a5418
Jacob Turner <j.israel.turner@gmail.com>
2021-03-28 22:49:34 -0600

    Run Prettier on /example

--- example/index.js
+++ example/index.js
@@ -9,7 +9,7 @@ import { name as appName } from './app.json';
 AppRegistry.registerComponent(appName, () => App);
 
 if (Platform.OS === 'web') {
-  AppRegistry.runApplication(appName, {
-      rootTag: document.getElementById('root'),
-  });
+	AppRegistry.runApplication(appName, {
+		rootTag: document.getElementById('root'),
+	});
 }
