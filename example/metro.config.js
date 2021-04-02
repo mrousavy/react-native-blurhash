2d5b033fe70842ecec266e46b9a7de0f7baeb13e
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-02 10:20:45 +0200

    Fix config to add workflows

--- example/metro.config.js
+++ example/metro.config.js
@@ -11,12 +11,23 @@
 // https://github.com/brodybits/create-react-native-module/issues/232
 
 const path = require('path');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const escape = require('escape-string-regexp');
+const pak = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+
+const modules = Object.keys({
+	...pak.peerDependencies,
+});
 
 module.exports = {
 	// workaround for an issue with symlinks encountered starting with
 	// metro@0.55 / React Native 0.61
 	// (not needed with React Native 0.60 / metro@0.54)
 	resolver: {
+		blacklistRE: exclusionList(modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))),
+
 		extraNodeModules: new Proxy({}, { get: (_, name) => path.resolve('.', 'node_modules', name) }),
 	},
 
