405988652a043f2a7d0d1d2bc02923ddde4f55a8
Janic Duplessis <janicduplessis@gmail.com>
2024-01-27 13:35:37 -0500

    Android impl

--- android/src/main/java/com/mrousavy/blurhash/BlurhashImageView.kt
+++ android/src/main/java/com/mrousavy/blurhash/BlurhashImageView.kt
@@ -2,13 +2,8 @@ package com.mrousavy.blurhash
 
 import android.content.Context
 import android.graphics.Bitmap
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.ReactContext
-import com.facebook.react.bridge.WritableMap
-import com.facebook.react.uimanager.events.RCTEventEmitter
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-
 
 internal class BlurhashCache(private val _blurhash: String?, private val _decodeWidth: Int, private val _decodeHeight: Int, private val _decodePunch: Float) {
     fun isDifferent(blurhash: String?, decodeWidth: Int, decodeHeight: Int, decodePunch: Float): Boolean {
@@ -42,18 +37,16 @@ class BlurhashImageView(context: Context): androidx.appcompat.widget.AppCompatIm
     var decodeHeight = 32
     var decodePunch = 1.0f
     var decodeAsync = false
-    private var _cachedBlurhash: BlurhashCache? = null
-    private var _mainThreadId = Thread.currentThread().id
-    private var _bitmap: Bitmap? = null
+    var onLoadStart: (() -> Unit)? = null
+    var onLoadEnd: (() -> Unit)? = null
+    var onLoadError: ((String?) -> Unit)? = null
 
-    private fun getThreadDescriptor(): String {
-        return if (Thread.currentThread().id == this._mainThreadId) "main"
-        else "separate"
-    }
+    private var _cachedBlurhash: BlurhashCache? = null
+    private var _bitmap: Bitmap? = null
 
     private fun renderBlurhash() {
         try {
-            emitBlurhashLoadStart()
+            onLoadStart?.invoke()
             if (blurhash == null) {
                 throw Exception("The provided Blurhash string must not be null!")
             }
@@ -71,10 +64,10 @@ class BlurhashImageView(context: Context): androidx.appcompat.widget.AppCompatIm
                 throw Exception("The provided Blurhash string was invalid.")
             }
             setImageBitmap(_bitmap)
-            emitBlurhashLoadEnd()
+            onLoadEnd?.invoke()
         } catch (e: Exception) {
             setImageBitmap(null)
-            emitBlurhashLoadError(e.message)
+            onLoadError?.invoke(e.message)
         }
     }
 
@@ -104,23 +97,6 @@ class BlurhashImageView(context: Context): androidx.appcompat.widget.AppCompatIm
         } finally {
             _cachedBlurhash = BlurhashCache(this.blurhash, this.decodeWidth, this.decodeHeight, this.decodePunch)
         }
-    }
-
-    private fun emitBlurhashLoadStart() {
-        val reactContext = context as ReactContext
-        reactContext.getJSModule(RCTEventEmitter::class.java).receiveEvent(id, "blurhashLoadStart", null)
-    }
-
-    private fun emitBlurhashLoadEnd() {
-        val reactContext = context as ReactContext
-        reactContext.getJSModule(RCTEventEmitter::class.java).receiveEvent(id, "blurhashLoadEnd", null)
-    }
-
-    private fun emitBlurhashLoadError(message: String?) {
-        val event = Arguments.createMap()
-        event.putString("message", message)
-        val reactContext = context as ReactContext
-        reactContext.getJSModule(RCTEventEmitter::class.java).receiveEvent(id, "blurhashLoadError", event)
     }
 
     companion object {
