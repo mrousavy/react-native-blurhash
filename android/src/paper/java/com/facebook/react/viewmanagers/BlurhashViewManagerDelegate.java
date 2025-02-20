bb45432b83d733e635c8d540d5002d2e20a95398
Brandon Fitzwater <7604441+iM-GeeKy@users.noreply.github.com>
2025-02-20 06:49:53 -0500

    fix: Fix react-native 0.77 build on Android (#202)

--- android/src/paper/java/com/facebook/react/viewmanagers/BlurhashViewManagerDelegate.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/BlurhashViewManagerDelegate.java
@@ -12,9 +12,10 @@ package com.facebook.react.viewmanagers;
 import android.view.View;
 import androidx.annotation.Nullable;
 import com.facebook.react.uimanager.BaseViewManagerDelegate;
-import com.facebook.react.uimanager.BaseViewManagerInterface;
+import com.facebook.react.uimanager.BaseViewManager;
+import com.facebook.react.uimanager.LayoutShadowNode;
 
-public class BlurhashViewManagerDelegate<T extends View, U extends BaseViewManagerInterface<T> & BlurhashViewManagerInterface<T>> extends BaseViewManagerDelegate<T, U> {
+public class BlurhashViewManagerDelegate<T extends View, U extends BaseViewManager<T, ? extends LayoutShadowNode> & BlurhashViewManagerInterface<T>> extends BaseViewManagerDelegate<T, U> {
   public BlurhashViewManagerDelegate(U viewManager) {
     super(viewManager);
   }
